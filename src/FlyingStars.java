1819f8eea936a37c9dea0823bed2bb0fc03d930d
SANA AMMAR <sanataffor@gmail.com>
2024-11-28 21:44:27 +0300

    Added classes

---
+++ src/FlyingStars.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FlyingStars{
+    
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+    }
+
+}
